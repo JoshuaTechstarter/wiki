2b82bdae254414d9be5874cebe3b14dd328bede8
Joshua Anders <joshua.anders@tn.techstarter.de>
2024-12-11 14:39:07 +0100

    Updated script_tests.py and script_homework.py

---
+++ script_homework.py
@@ -0,0 +1,18 @@
+
+print("Aufgabe 1: Einfache Mathematik")
+# Variablen
+zahl1 = 34
+zahl2 = 67
+
+# Berechnung
+print("Zahl 1 + Zahl 2:", zahl1 + zahl2)
+print("Zahl 1 - Zahl 2:", zahl1 - zahl2)
+print("Zahl 1 * Zahl 2:", zahl1 * zahl2)
+print("Zahl 1 / Zahl 2:", zahl2 / zahl1)
+
+print("Aufgabe 2: Durchschnitt berechnen")
+
+# Variablen
+note1 = 
+note2 = 
+note3 = 
--- script_tests.py
+++ script_tests.py
@@ -9,3 +9,9 @@ print(first_name+ " " +last_name)
 print("Zahl 1 + Zahl 2:", zahl1 + zahl2)
 print("Zahl 1 * Zahl 2:", zahl1 * zahl2)
 print("Zahl 1 / Zahl 2:", zahl2 / zahl1)
+
+name = input("What is your name?"" ")
+print("Hello" " " +name)
+
+age = input("What is your age?"" ")
+print("Your age:" " " +age)
