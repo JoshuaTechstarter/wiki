58715aec673669bb0d3bdd0aa2d3c2746cdf7a2d
Joshua Anders <joshua.anders@tn.techstarter.de>
2024-12-11 12:49:35 +0100

    Create script_tests.py

---
+++ script_tests.py
@@ -0,0 +1,11 @@
+first_name = "Joshua"
+last_name = "Anders"
+
+zahl1 = 2
+zahl2 = 10
+
+print(first_name+ " " +last_name)
+
+print("Zahl 1 + Zahl 2:", zahl1 + zahl2)
+print("Zahl 1 * Zahl 2:", zahl1 * zahl2)
+print("Zahl 1 / Zahl 2:", zahl2 / zahl1)
