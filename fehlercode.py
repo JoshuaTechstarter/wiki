8222af6a22ad2a57060610b8bbee5db97722ce27
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-02-07 11:22:39 +0100

    Create fehlercode.py

---
+++ fehlercode.py
@@ -0,0 +1,52 @@
+import nath
+
+def calculate_area(radius)
+    area = math.pi * radius ** 2
+    return area
+
+def print_area(radius):
+    area = calculate_area(r)
+    print("Die Fläche des Kreises mit Radius " + radius + " beträgt: " + area)
+
+r = input("Gib den Radius ein: ")
+print_area(r)
+
+zahlen = [1, 2, 3, 4, 5]
+for i in zahlen
+    print "Zahl: " + i
+
+if zahlen[5] == 5:
+    print("Fünf gefunden!")
+
+dictionary = {"Name": "Max", "Alter": 25, "Stadt": "Berlin"}
+print("Name: " + dictionary["name"])
+
+def add_numbers(a, b)
+    return a + b
+
+ergebnis = add_numbers(10)
+print("Das Ergebnis ist: " + ergebnis)
+
+for j in range(3, -1):
+    print("Wert von j: " + str(j))
+
+while j > 0:
+    print("Countdown: " + j)
+    j -= 1
+
+def check_even(num):
+    if num % 2 = 0:
+    print(num + " ist gerade")
+    else
+    print(num + " ist ungerade")
+
+check_even(4)
+
+liste = [10, 20, 30, 40]
+print("Letztes Element: " + liste[4])
+
+def multiply(x, y):
+return x * y
+
+result = multiply(5)
+print("Multiplikationsergebnis: " result)
